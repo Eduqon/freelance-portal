36cc9502845382ec36cc8c141d4f526faaffeea8
Rohan Taneja <rohtanej1@WKMZTCF50241.global.publicisgroupe.net>
2023-07-28 11:10:52 +0530

    remove the IsVerify component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import SubmitQuote from "../src/pages/SubmitQuote";
 import ExpertOpertorChat from "./pages/ExpertOperatorChat";
 import Portal from "./pages/Portal";
 import AdminLogin from "./pages/Login";
-import Isverify from "./pages/IsVerify";
 import AssignmentDetails from "./pages/AssignmentDetails";
 
 ReactDOM.render(
@@ -21,7 +20,6 @@ ReactDOM.render(
             path="/assignment_details/:assignmentID"
             element={<AssignmentDetails />}
           />
-          <Route path="/verify/:email" element={<Isverify />} />
           <Route path="/:assignmentID/:expertID" element={<SubmitQuote />} />
           <Route
             path="/:assignmentID/:expertID/expert_Operator_Chat"
